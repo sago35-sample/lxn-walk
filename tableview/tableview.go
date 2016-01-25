750ecb7c03611834ec3709fc4bfd79b2c94b9b60
sago35 <sago35@gmail.com>
2016-01-25 23:43:02 +0900

    最低限のデータ表示まで作成

--- tableview/tableview.go
+++ tableview/tableview.go
@@ -1,13 +1,70 @@
 package main
 
 import (
+	"github.com/lxn/walk"
 	. "github.com/lxn/walk/declarative"
 )
 
+type Person struct {
+	Index int
+	Name  string
+	Age   int
+}
+
+type PersonModel struct {
+	walk.TableModelBase
+	items []*Person
+}
+
+func (m *PersonModel) RowCount() int {
+	return len(m.items)
+}
+
+func (m *PersonModel) Value(row, col int) interface{} {
+	item := m.items[row]
+
+	switch col {
+	case 0:
+		return item.Index
+	case 1:
+		return item.Name
+	case 2:
+		return item.Age
+	}
+	panic("unexpected col")
+}
+
+func NewPersonModel() *PersonModel {
+	m := new(PersonModel)
+	m.items = make([]*Person, 3)
+
+	m.items[0] = &Person{
+		Index: 0,
+		Name: "山田",
+		Age: 20,
+	}
+
+	m.items[1] = &Person{
+		Index: 1,
+		Name: "鈴木",
+		Age: 21,
+	}
+
+	m.items[2] = &Person{
+		Index: 2,
+		Name: "田中",
+		Age: 22,
+	}
+
+	return m
+}
+
 func main() {
+	model := NewPersonModel()
+
 	MainWindow{
-		Title: "TableViewサンプル",
-		Size:  Size{800, 600},
+		Title:  "TableViewサンプル",
+		Size:   Size{800, 600},
 		Layout: VBox{},
 		Children: []Widget{
 			TableView{
@@ -16,6 +73,7 @@ func main() {
 					{Title: "名前"},
 					{Title: "年齢"},
 				},
+				Model: model,
 			},
 		},
 	}.Run()
