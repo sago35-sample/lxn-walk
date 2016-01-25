48897806599034d29783741b7762747281c1a331
sago35 <sago35@gmail.com>
2016-01-25 23:30:52 +0900

    最低限の見た目を作成

--- tableview/tableview.go
+++ tableview/tableview.go
@@ -6,5 +6,17 @@ import (
 
 func main() {
 	MainWindow{
+		Title: "TableViewサンプル",
+		Size:  Size{800, 600},
+		Layout: VBox{},
+		Children: []Widget{
+			TableView{
+				Columns: []TableViewColumn{
+					{Title: "#"},
+					{Title: "名前"},
+					{Title: "年齢"},
+				},
+			},
+		},
 	}.Run()
 }
